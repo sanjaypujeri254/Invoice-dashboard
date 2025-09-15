fba03b180f383335419f39622a13e8b6f0d3c4b7
Sanjay S .P <sanjaypujeri254@gmail.com>
2025-09-15 11:58:28 +0530

    changes in package.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
-import InvoiceDashboard from "./Component/InvoiceDashboard"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import InvoiceDashboard from "./Component/InvoiceDashboard";
 function App() {
-  return <InvoiceDashboard/>
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<InvoiceDashboard />} />
+      </Routes>
+    </Router>
+  );
 }
+
 export default App;
